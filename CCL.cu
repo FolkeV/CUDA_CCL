5c3ac398bd3a8286873e35c3ad9dd88a0c662b94
elheck <42242163+elheck@users.noreply.github.com>
2021-09-30 09:45:13 +0200

    Proposed fix in documentation

--- CCL.cu
+++ CCL.cu
@@ -76,7 +76,7 @@ __global__ void init_labels(unsigned int* g_labels, const unsigned char *g_image
 
 		// Initialise Label
 		// Label will be chosen in the following order:
-		// NW > N > NE > E > current position
+		// NW > N > NE > W > current position
 		label = (nyxm1)   ?  iy   *numCols + ix-1 : iy*numCols + ix;
 		label = (nym1xp1) ? (iy-1)*numCols + ix+1 : label;
 		label = (nym1x)   ? (iy-1)*numCols + ix   : label;
